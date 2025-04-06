97de0702e4677cabfb4e24d4f98e236849fd74e5
aladi.timera1605@gmail.com <aladi.timera1605@gmail.com>
2025-04-06 15:55:12 +0200

    not content if lg media queries + indent code

--- frontend/src/pages/profile/GestionPieces.jsx
+++ frontend/src/pages/profile/GestionPieces.jsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
+import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
 
 const GestionPieces = () => {
-    return (
-       <div className='gestion_layout'>
-                  <nav className='nav_bar_pieces'>
-         
-         <NavLink to='/compte/gestion_pieces/add_piece'>Ajouter une piéce d'identité utopid</NavLink>
-         <NavLink to='/compte/gestion_pieces/my_pieces'>Mes pièces</NavLink>
-         <NavLink to='/compte/gestion_pieces/my_stats'>Mes statistiques</NavLink>
+  return (
+    <div className="gestion_layout">
+      <nav className="nav_bar_pieces">
+        <NavLink to="/compte/gestion_pieces/add_piece">
+          Ajouter une piéce d'identité utopid
+        </NavLink>
+        <NavLink to="/compte/gestion_pieces/my_pieces">Mes pièces</NavLink>
+        <NavLink to="/compte/gestion_pieces/my_stats">Mes statistiques</NavLink>
+      </nav>
 
- 
-         </nav>
-
-<div className='nav_bar_pieces_container'>
-    <Outlet />
-</div>
-</div>
-        
-    );
+      <div className="nav_bar_pieces_container">
+        <Outlet />
+      </div>
+    </div>
+  );
 };
 
 export default GestionPieces;
--- frontend/src/pages/public/PublicLayout.jsx
+++ frontend/src/pages/public/PublicLayout.jsx
@@ -1,24 +1,27 @@
-import { Outlet } from "react-router-dom"
-import Footer from "@c_public/Footer"
-
-
-
-
-
-
+import { Outlet } from "react-router-dom";
+import Footer from "@c_public/Footer";
+import logo from "@/images/6W7g.gif";
 const PublicLayout = () => {
+  return (
+    <>
+      <div className="lg:hidden flex  flex-col  p-5 bg-dark-utopid h-[100vh] text-light-utopid justify-center items-center gap-5 text-center">
+        <div className="text-5xl">
+          Désolé, Utopid est uniquement accessible sur ordinateur{" "}
+        </div>
 
-    return (
-<>
-        <div className="layout_public bg-gray-100 h-[100vh] flex flex-col justify-between"> 
-        
+        <img src={logo} />
+        <p className="text-2xl">
+          Pour bénéficier de l'application, veillez utiliser un ordinateur. Nous
+          vous remercions de cotre compréhension{" "}
+        </p>
+      </div>
+
+      <div className="layout_public bg-gray-100 h-[100vh]  flex-col justify-between hidden lg:flex">
         <Outlet />
         <Footer />
-        </div>
-       
-</>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-
-export default PublicLayout
+export default PublicLayout;
