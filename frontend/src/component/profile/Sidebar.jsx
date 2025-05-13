dff232ba996a43c79a6be684b494544dfe773323
aladi.timera1605@gmail.com <aladi.timera1605@gmail.com>
2025-05-13 21:11:14 +0200

    amlioration sidebar

--- frontend/src/component/profile/Sidebar.jsx
+++ frontend/src/component/profile/Sidebar.jsx
@@ -5,15 +5,16 @@ import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
 import FormatListNumberedIcon from "@mui/icons-material/FormatListNumbered";
 import TimelineIcon from "@mui/icons-material/Timeline";
 import MenuIcon from "@mui/icons-material/Menu";
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
 const Sidebar = () => {
   return (
-    <nav className="nav_bar_profile  h-[100%] top flex flex-col  w-[10%]  fixed  bg-dark-utopid  font-utopid justify-start font-bold">
-      <div className=" bg-light-utopid h-[45px]   text-center text-dark-utopid border-b-6 border-b-white p-2 cursor-pointer">
-        <p>UTOPID</p>
+    <nav className="nav_bar_profile  h-[100%] top flex flex-col  w-[10%]  fixed  font-utopid justify-between font-bold">
+      <div className=" bg-intermediaire-utopid h-[45px]   text-center text-dark-utopid border-b-6 border-b-white p-2 cursor-pointer">
+ <p>UTOPID</p>
       </div>
 
       <NavLink
-        className="border-b-1  hover:bg-light-utopid hover:text-dark-utopid  bg-dark-utopid border-light-utopid text-light-utopid  h-38 text-center flex flex-col justify-center items-center"
+        className=" hover:bg-intermediaire-utopid hover:text-dark-utopid  bg-dark-utopid border-intermediaire-utopid text-light-utopid  h-38 text-center flex flex-col justify-center items-center"
         to="/compte"
       >
         <SpaceDashboardIcon />
@@ -21,7 +22,7 @@ const Sidebar = () => {
       </NavLink>
 
       <NavLink
-        className="border-b-1     hover:bg-light-utopid hover:text-dark-utopid  bg-dark-utopid  border-light-utopid text-light-utopid  h-38 text-center flex flex-col justify-center items-center"
+        className=" hover:bg-intermediaire-utopid hover:text-dark-utopid  bg-dark-utopid  border-intermediaire-utopid text-light-utopid  h-38 text-center flex flex-col justify-center items-center"
         to="/compte/add_piece"
       >
         <SettingsApplicationsIcon />
@@ -29,7 +30,7 @@ const Sidebar = () => {
       </NavLink>
 
       <NavLink
-        className="border-b-1  hover:bg-light-utopid hover:text-dark-utopid   bg-dark-utopid border-light-utopid text-light-utopid  h-38 text-center flex flex-col justify-center items-center"
+        className=" hover:bg-intermediaire-utopid hover:text-dark-utopid   bg-dark-utopid border-intermediaire-utopid text-light-utopid  h-38 text-center flex flex-col justify-center items-center"
         to="/compte/my_pieces"
       >
         <FormatListNumberedIcon />
@@ -37,7 +38,7 @@ const Sidebar = () => {
       </NavLink>
 
       <NavLink
-        className="border-b-1  hover:bg-light-utopid hover:text-dark-utopid   bg-dark-utopid border-light-utopid text-light-utopid  h-38 text-center flex flex-col justify-center items-center"
+        className=" hover:bg-intermediaire-utopid hover:text-dark-utopid   bg-dark-utopid border-intermediaire-utopid text-light-utopid  h-38 text-center flex flex-col justify-center items-center"
         to="/compte/utopid_history"
       >
         <TimelineIcon />
@@ -45,22 +46,26 @@ const Sidebar = () => {
       </NavLink>
 
       <NavLink
-        className="border-b-1  hover:bg-light-utopid hover:text-dark-utopid   bg-dark-utopid border-light-utopid text-light-utopid  h-38 text-center flex flex-col justify-center items-center"
+        className=" hover:bg-intermediaire-utopid hover:text-dark-utopid   bg-dark-utopid border-intermediaire-utopid text-light-utopid  h-38 text-center flex flex-col justify-center items-center"
         to="/compte/utopid_history"
       >
-        <TimelineIcon />
+        <ManageAccountsIcon />
         <div>Mon compte</div>
       </NavLink>
 
-
-      <NavLink
-        className="h-[40px] text-center flex flex-col justify-center items-center text-dark-utopid bg-light-utopid"
+            <NavLink
+              className="h-[40px] text-center border-t-6 border-t-white  flex flex-col justify-center items-center font-mono font-medium text-xs text-dark-utopid bg-intermediaire-utopid"
+              to="/compte/utopid_history"
+            >
+                <p>Copyright@utopid</p>
+            </NavLink>
+      
+{/*       <NavLink
+        className="h-[40px] text-center border-t-6 border-t-white  flex flex-col justify-center items-center font-mono font-medium text-xs text-dark-utopid bg-light-utopid"
         to="/compte/utopid_history"
       >
           <p>Copyright@utopid</p>
       </NavLink>
-
-      {/* 
       <NavLink
         className="    bg-[url('./images/cactus.jpg')] bg-cover w-[100%]  bg-no-repeat    h-20 text-center flex flex-col justify-center items-center"
         to="/compte/utopid_history"
